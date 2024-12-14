37e10b5a178119ccc50382f0effbfc11b6007e3b
busami-star <victorbusami1@gmail.com>
2024-12-14 18:18:22 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Main from './components/Main.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import './components/header.css'
+import './components/main.css'
+import './components/footer.css'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
